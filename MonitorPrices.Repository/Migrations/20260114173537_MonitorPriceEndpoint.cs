cf93950f07700088ac54df1762750bd3dfe01733
Yeray <111679635+YerayBA@users.noreply.github.com>
2026-01-14 18:36:26 +0100

    migracion

---
+++ MonitorPrices.Repository/Migrations/20260114173537_MonitorPriceEndpoint.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MonitorPrices.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class MonitorPriceEndpoint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Precio",
+                table: "Products");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaUltimaComprobación",
+                table: "Products",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "PrecioActual",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PrecioUltimo",
+                table: "Products",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FechaUltimaComprobación",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "PrecioActual",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "PrecioUltimo",
+                table: "Products");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Precio",
+                table: "Products",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
--- MonitorPrices.Services/obj/Debug/net10.0/MonitorPrices.Services.AssemblyInfo.cs
+++ MonitorPrices.Services/obj/Debug/net10.0/MonitorPrices.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonitorPrices.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fb1f10167119b2d85b4a5ac790b3307895529f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48135d07b183b0f86ce4e3898a9030e7e01f4100")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonitorPrices.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonitorPrices.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
